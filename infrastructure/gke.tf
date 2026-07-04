01a6a42995c78e92af98aef3e2d9ee20cb788aa1
Saadnaanaiy <saadnaanaiy@gmail.com>
2026-07-04 16:04:20 +0100

    fix: use boot_disk block in node pool for provider v6 compatibility

--- infrastructure/gke.tf
+++ infrastructure/gke.tf
@@ -52,8 +52,10 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_config {
     machine_type = var.gke_machine_type
-    disk_type    = "pd-standard"
-    disk_size_gb = 50
+    boot_disk {
+      disk_type    = "pd-standard"
+      disk_size_gb = 50
+    }
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
